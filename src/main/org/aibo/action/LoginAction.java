ec2ada806e806c790586b511592e08e92c8be4bc
squirrelschen <squirrelschen@gmail.com>
2018-01-25 17:41:26 +0800

    Improve login function

--- src/main/org/aibo/action/LoginAction.java
+++ src/main/org/aibo/action/LoginAction.java
@@ -1,10 +1,12 @@
 package org.aibo.action;
 
 import org.aibo.service.LoginService;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 
 /**
  * Created by Squirrel-Chen on 2018/1/25.
@@ -15,10 +17,25 @@ public class LoginAction {
 
     @Autowired
     private LoginService loginService;
+    private static Logger log = Logger.getLogger(LoginAction.class);
+
 
     @RequestMapping("/login")
     public String login(@RequestParam("username")String username,@RequestParam("password")String password)
     {
+
+        log.info("This is username "+":        "+username+"      :" +"password"+":      "+password+"     :");
+
+
+
+        if(username.equals("")||password.equals(""))
+        {
+            log.debug("This is ' ' string ");
+        }
+        if(username.equals(null)||password.equals(null))
+        {
+            log.debug("This is null string ");
+        }
 
         if(loginService.login(username,password))
         {
