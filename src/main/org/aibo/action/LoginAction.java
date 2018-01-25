86fd6790ea7b61ca61f052ce97025445baf35b1e
squirrelschen <squirrelschen@gmail.com>
2018-01-25 16:39:40 +0800

    Modify loginaction

--- src/main/org/aibo/action/LoginAction.java
+++ src/main/org/aibo/action/LoginAction.java
@@ -1,7 +1,22 @@
 package org.aibo.action;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 /**
  * Created by Squirrel-Chen on 2018/1/25.
  */
+
+@Controller
 public class LoginAction {
+
+    @RequestMapping("/login")
+    public String login(@RequestParam("username")String username,@RequestParam("password")String password)
+    {
+        return "success";
+    }
+
+
+
 }
