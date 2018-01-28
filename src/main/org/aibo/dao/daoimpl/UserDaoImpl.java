3ed39eea0efd75275ff1c8e78d986ff11e993e39
squirrelschen <squirrelschen@gmail.com>
2018-01-28 20:01:36 +0800

    implments user dao,mapper,service

--- src/main/org/aibo/dao/daoimpl/UserDaoImpl.java
+++ src/main/org/aibo/dao/daoimpl/UserDaoImpl.java
@@ -1,9 +1,23 @@
 package org.aibo.dao.daoimpl;
 
+import org.aibo.bean.User;
 import org.aibo.dao.UserDao;
+import org.aibo.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 
 /**
  * Created by Squirrel-Chen on 2018/1/28.
  */
+@Repository
 public class UserDaoImpl implements UserDao {
+
+    @Autowired
+    private UserMapper userMapper;
+
+
+    public User getUserInfo(String username) {
+        return userMapper.getUserInfo(username);
+    }
 }
--- src/main/org/aibo/mapper/UserMapper.java
+++ src/main/org/aibo/mapper/UserMapper.java
@@ -1,7 +1,13 @@
 package org.aibo.mapper;
 
+import org.aibo.bean.User;
+import org.springframework.stereotype.Component;
+
 /**
  * Created by Squirrel-Chen on 2018/1/28.
  */
+@Component
 public interface UserMapper {
+
+    public User getUserInfo(String  username);
 }
