177ac3b55a63acec2ccccbbe5488d32bcfa72d39
squirrelschen <squirrelschen@gmail.com>
2018-02-03 10:27:48 +0800

    user_icon upload function

--- src/main/org/aibo/action/FileUploadController.java
+++ src/main/org/aibo/action/FileUploadController.java
@@ -21,32 +21,24 @@ import java.io.*;
 @Controller
 public class FileUploadController {
 
-    /**
-     * 这里这里用的是MultipartFile[] myfiles参数,所以前台就要用<input type="file" name="myfiles"/>
-     * 上传文件完毕后返回给前台[0`filepath],0表示上传成功(后跟上传后的文件路径),1表示失败(后跟失败描述)
-     */
     @RequestMapping(value="/fileUpload",method = RequestMethod.POST)
     @ResponseBody
-    public Object addUser(@RequestParam MultipartFile[] uploadfiles, HttpServletRequest request, HttpServletResponse response) throws IOException{
-        //可以在上传文件的同时接收其它参数
+    public Object fileUpload(@RequestParam MultipartFile[] uploadfiles, HttpServletRequest request, HttpServletResponse response) throws IOException{
+
         System.out.println("收到用户["  + "]的文件上传请求");
-        //如果用的是Tomcat服务器，则文件会上传到\\%TOMCAT_HOME%\\webapps\\YourWebProject\\upload\\文件夹中
-        //这里实现文件上传操作用的是commons.io.FileUtils类,它会自动判断/upload是否存在,不存在会自动创建
-        String realPath = request.getSession().getServletContext().getRealPath("/upload");
-        //设置响应给前台内容的数据格式
-        response.setContentType("text/plain; charset=UTF-8");
-        //设置响应给前台内容的PrintWriter对象
-        PrintWriter out = response.getWriter();
+        String realPath = "E://picture";
+        String str=new String();
+
+        boolean flag=false;
+        int size=uploadfiles.length;
+        int count=0;
+
         //上传文件的原名(即上传前的文件名字)
         String originalFilename = null;
-        //如果只是上传一个文件,则只需要MultipartFile类型接收文件即可,而且无需显式指定@RequestParam注解
-        //如果想上传多个文件,那么这里就要用MultipartFile[]类型来接收文件,并且要指定@RequestParam注解
-        //上传多个文件时,前台表单中的所有<input type="file"/>的name都应该是myfiles,否则参数里的myfiles无法获取到所有上传的文件
+
         for(MultipartFile myfile : uploadfiles){
             if(myfile.isEmpty()){
-                out.print("1`请选择文件后上传");
-                out.flush();
-                return null;
+
             }else{
                 originalFilename = myfile.getOriginalFilename();
                 System.out.println("文件原名: " + originalFilename);
@@ -55,31 +47,25 @@ public class FileUploadController {
                 System.out.println("文件类型: " + myfile.getContentType());
                 System.out.println("========================================");
                 try {
-                    //这里不必处理IO流关闭的问题,因为FileUtils.copyInputStreamToFile()方法内部会自动把用到的IO流关掉
                     //此处也可以使用Spring提供的MultipartFile.transferTo(File dest)方法实现文件的上传
                     FileUtils.copyInputStreamToFile(myfile.getInputStream(), new File(realPath, originalFilename));
+                    String string="{'status':'true'}";
+                    count=count+1;
                 } catch (IOException e) {
                     System.out.println("文件[" + originalFilename + "]上传失败,堆栈轨迹如下");
                     e.printStackTrace();
-                    out.print("1`文件上传失败，请重试！！");
-                    out.flush();
-                    String string="{'status':'true'}";
-                    return JSON.parse(string);
+                    String string="{'status':'false'}";
                 }
             }
         }
-        //此时在Windows下输出的是[D:\Develop\apache-tomcat-6.0.36\webapps\AjaxFileUpload\\upload\愤怒的小鸟.jpg]
-        //System.out.println(realPath + "\\" + originalFilename);
-        //此时在Windows下输出的是[/AjaxFileUpload/upload/愤怒的小鸟.jpg]
-        //System.out.println(request.getContextPath() + "/upload/" + originalFilename);
-        //不推荐返回[realPath + "\\" + originalFilename]的值
-        //因为在Windows下<img src="file:///D:/aa.jpg">能被firefox显示,而<img src="D:/aa.jpg">firefox是不认的
-        out.print("0`" + request.getContextPath() + "/upload/" + originalFilename);
-        out.flush();
-        return null;
+
+        if(count!=0)
+        {
+            str="{'flag':'true'}";
+            return JSON.parse(str);
+        }
+        str="{'flag':'false'}";
+        return JSON.parse(str);
     }
-
-
-
 
 }
