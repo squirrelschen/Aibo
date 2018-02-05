7b9c8598f80339ff825bb38bb572e62aef25d7ac
squirrelschen <squirrelschen@gmail.com>
2018-02-05 19:32:30 +0800

    Add regular match js code

--- src/main/webapp/js/page/article.js
+++ src/main/webapp/js/page/article.js
@@ -4,10 +4,17 @@
 
 $(document).ready(function () {
 
+    var article_id=console.log(getQueryString("id"));
+    if(article_id==null)
+    {
+        article_id=-1;
+    }
+
+
     //ajax请求获取博文内容
     $.ajax({
         url:"article",
-        data:{"id":1},
+        data:{"id":article_id},
         dataType:"json",
         method:"get",
         success:function (data) {
@@ -15,3 +22,9 @@ $(document).ready(function () {
         }
     })
 })
+
+function getQueryString(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]); return null;
+}
