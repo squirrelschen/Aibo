da0676d9e8a41e28460a5658fba119abc2ecf931
squirrelschen <squirrelschen@gmail.com>
2018-02-05 19:21:04 +0800

    Changes article.js in order to run demo

--- src/main/webapp/js/page/article.js
+++ src/main/webapp/js/page/article.js
@@ -6,13 +6,12 @@ $(document).ready(function () {
 
     //ajax请求获取博文内容
     $.ajax({
-        url:"",
-        data:{},
+        url:"article",
+        data:{"id":1},
         dataType:"json",
         method:"get",
         success:function (data) {
             console.log(data);
         }
     })
-
 })
