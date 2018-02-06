3bd0bf42411bfdea9c50fe5ddb259b8841ff9a83
squirrelschen <squirrelschen@gmail.com>
2018-02-06 10:56:25 +0800

    Add insertArticle function in mapper

--- src/main/org/aibo/mapper/ArticleMapper.java
+++ src/main/org/aibo/mapper/ArticleMapper.java
@@ -15,4 +15,5 @@ public interface ArticleMapper {
     public ArrayList<Article> selectUserArticleByPage(@Param("user_id")int user_id, @Param("pageNum")int pageNum, @Param("pageSize")int pageSize);
     public Article selectArticleById(int article_id);
     public int updateArticleById(Article article);
+    public int insertArticle(Article article);
 }
