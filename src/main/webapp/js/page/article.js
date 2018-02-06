1da18a1d165941832cf24ccbd894d8b141340cb8
squirrelschen <squirrelschen@gmail.com>
2018-02-06 10:33:52 +0800

    Update article.js css style

--- src/main/webapp/js/page/article.js
+++ src/main/webapp/js/page/article.js
@@ -4,7 +4,7 @@
 
 $(document).ready(function () {
 
-    var article_id=console.log(getQueryString("id"));
+    var article_id=getQueryString("id");
     if(article_id==null)
     {
         article_id=-1;
@@ -19,6 +19,7 @@ $(document).ready(function () {
         method:"get",
         success:function (data) {
             console.log(data);
+            loadData(data.article);
         }
     })
 })
@@ -27,4 +28,17 @@ function getQueryString(name) {
     var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
     var r = window.location.search.substr(1).match(reg);
     if (r != null) return unescape(r[2]); return null;
+}
+
+function loadData(data)
+{
+    $("#article_info").empty();
+    $("#article_info").append("<div class='card large'><div class='card-image'>" +
+        "<img src=''>" +
+        "<span class='card-title'> "+data.article_title+"</span>"+
+            "<a class='btn-floating halfway-fab waves-effect waves-light red'><i class='material-icons'>edit</i> </a>"+
+            "</div>"+
+            "<div class='card-content'><p> "+data.article_context+"</p></div></div>"
+    );
+
 }
