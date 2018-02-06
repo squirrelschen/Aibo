70249e23f4c91513a895642c6c5d19beb6019d31
squirrelschen <squirrelschen@gmail.com>
2018-02-06 16:02:15 +0800

    Comments ArticleMapper all functions

--- src/main/org/aibo/mapper/ArticleMapper.java
+++ src/main/org/aibo/mapper/ArticleMapper.java
@@ -12,9 +12,9 @@ import java.util.ArrayList;
 @Component
 public interface ArticleMapper {
 
-    public ArrayList<Article> selectUserArticleByPage(@Param("user_id")int user_id, @Param("pageNum")int pageNum, @Param("pageSize")int pageSize);
-    public Article selectArticleById(int article_id);
-    public int updateArticleById(Article article);
-    public int insertArticle(Article article);
-    public int deleteArticleById(int article_id);
+    public ArrayList<Article> selectUserArticleByPage(@Param("user_id")int user_id, @Param("pageNum")int pageNum, @Param("pageSize")int pageSize); //获取博文信息时候分页
+    public Article selectArticleById(int article_id);         //通过id获取对应博文信息
+    public int updateArticleById(Article article);            //更新对应id的博文信息
+    public int insertArticle(Article article);                //插入博文信息
+    public int deleteArticleById(int article_id);             //删除对应id博文信息
 }
