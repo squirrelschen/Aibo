96e84c3c35514a110742df010d9457d39a4ea51b
squirrelschen <squirrelschen@gmail.com>
2018-02-07 16:59:40 +0800

    Modified Article_Collection

--- src/main/org/aibo/bean/Article_Collection.java
+++ src/main/org/aibo/bean/Article_Collection.java
@@ -10,43 +10,43 @@ public class Article_Collection implements Serializable{
 
     private static final long serialVersionUID = 8918416247831526099L;
     private int collection_id;    //收藏的id
-    private int article_id;      //收藏的博文id
-    private int user_id;         //收藏的用户id
+    private int collection_article_id;      //收藏的博文id
+    private int collection_user_id;         //收藏的用户id
     private Date collection_date;  //收藏的时间
 
     /**
      *  Article_Collection getter setter 方法
      */
 
-    public int getUser_id() {
-        return user_id;
-    }
-
-    public void setUser_id(int user_id) {
-        this.user_id = user_id;
-    }
-
-    public int getArticle_id() {
-        return article_id;
+    public int getCollection_id() {
+        return collection_id;
     }
 
     public Date getCollection_date() {
         return collection_date;
     }
 
-    public int getCollection_id() {
-        return collection_id;
+    public int getCollection_article_id() {
+        return collection_article_id;
     }
 
-    public void setArticle_id(int article_id) {
-        this.article_id = article_id;
+    public int getCollection_user_id() {
+        return collection_user_id;
+    }
+
+    public void setCollection_id(int collection_id) {
+        this.collection_id = collection_id;
     }
 
     public void setCollection_date(Date collection_date) {
         this.collection_date = collection_date;
     }
 
-    public void setCollection_id(int collection_id) {
-        this.collection_id = collection_id;
+    public void setCollection_article_id(int collection_article_id) {
+        this.collection_article_id = collection_article_id;
+    }
+
+    public void setCollection_user_id(int collection_user_id) {
+        this.collection_user_id = collection_user_id;
     }
 }
