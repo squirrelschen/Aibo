54919b1e7538e10300528a6189a826d8321d5340
squirrelschen <squirrelschen@gmail.com>
2018-02-09 20:06:48 +0800

    MessageMapper.xml config with mybatis.xml

--- src/main/org/aibo/mapper/MessageMapper.java
+++ src/main/org/aibo/mapper/MessageMapper.java
@@ -1,7 +1,15 @@
 package org.aibo.mapper;
 
+import org.aibo.bean.Message;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
 /**
  * Created by Squirrel-Chen on 2018/2/9.
  */
+@Component
 public interface MessageMapper {
+
+    public ArrayList<Message> selectMessageByUserId(int user_id);
 }
