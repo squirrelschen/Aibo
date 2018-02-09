99bbf3aed0791c37c1b53fe64827bf94f0751e58
squirrelschen <squirrelschen@gmail.com>
2018-02-09 20:10:47 +0800

    MessageDao implments

--- src/main/org/aibo/dao/daoimpl/MessageDaoImpl.java
+++ src/main/org/aibo/dao/daoimpl/MessageDaoImpl.java
@@ -1,9 +1,23 @@
 package org.aibo.dao.daoimpl;
 
+import org.aibo.bean.Message;
 import org.aibo.dao.MessageDao;
+import org.aibo.mapper.MessageMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
 
 /**
  * Created by Squirrel-Chen on 2018/2/9.
  */
+@Repository
 public class MessageDaoImpl implements MessageDao {
+
+    @Autowired
+    private MessageMapper messageMapper;
+
+    public ArrayList<Message> selectMessageByUserId(int user_id) {
+        return messageMapper.selectMessageByUserId(user_id);
+    }
 }
