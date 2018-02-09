de37b3282b726730b8166d11462cde7bdbc3f6c6
squirrelschen <squirrelschen@gmail.com>
2018-02-09 19:59:18 +0800

    Create MessageDao and  MessageDaoImpl interface and class

---
+++ src/main/org/aibo/dao/MessageDao.java
@@ -0,0 +1,7 @@
+package org.aibo.dao;
+
+/**
+ * Created by Squirrel-Chen on 2018/2/9.
+ */
+public interface MessageDao {
+}
---
+++ src/main/org/aibo/dao/daoimpl/MessageDaoImpl.java
@@ -0,0 +1,9 @@
+package org.aibo.dao.daoimpl;
+
+import org.aibo.dao.MessageDao;
+
+/**
+ * Created by Squirrel-Chen on 2018/2/9.
+ */
+public class MessageDaoImpl implements MessageDao {
+}
