b373374ca81b5748cce3b7e0657073ca12c76ff2
squirrelschen <squirrelschen@gmail.com>
2018-02-09 20:01:33 +0800

    Create MessageService Interface and Class

---
+++ src/main/org/aibo/service/serviceimpl/MessageServiceImpl.java
@@ -0,0 +1,9 @@
+package org.aibo.service.serviceimpl;
+
+import org.aibo.service.MessageService;
+
+/**
+ * Created by Squirrel-Chen on 2018/2/9.
+ */
+public class MessageServiceImpl implements MessageService {
+}
