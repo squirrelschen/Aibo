6169c27e2360d4c8a564568471f0c18264273b7c
squirrelschen <squirrelschen@gmail.com>
2018-02-09 19:50:57 +0800

    Add getter setter function with Message.java

--- src/main/org/aibo/bean/Message.java
+++ src/main/org/aibo/bean/Message.java
@@ -15,5 +15,55 @@ public class Message {
     private String message_context;
     private Date message_date;
 
+    /**
+     *  Message getter setter 方法
+     */
 
+    public int getMessage_id() {
+        return message_id;
+    }
+
+    public int getMessage_user_id() {
+        return message_user_id;
+    }
+
+    public int getMessage_type() {
+        return message_type;
+    }
+
+    public Date getMessage_date() {
+        return message_date;
+    }
+
+    public String getMessage_context() {
+        return message_context;
+    }
+
+    public String getMessage_title() {
+        return message_title;
+    }
+
+    public void setMessage_id(int message_id) {
+        this.message_id = message_id;
+    }
+
+    public void setMessage_user_id(int message_user_id) {
+        this.message_user_id = message_user_id;
+    }
+
+    public void setMessage_type(int message_type) {
+        this.message_type = message_type;
+    }
+
+    public void setMessage_title(String message_title) {
+        this.message_title = message_title;
+    }
+
+    public void setMessage_date(Date message_date) {
+        this.message_date = message_date;
+    }
+
+    public void setMessage_context(String message_context) {
+        this.message_context = message_context;
+    }
 }
