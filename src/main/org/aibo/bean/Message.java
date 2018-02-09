89e0ad7f59dfeb1dfd2ad5351b70f41c06dbbd70
squirrelschen <squirrelschen@gmail.com>
2018-02-09 19:48:45 +0800

    Upload Message.java new file

---
+++ src/main/org/aibo/bean/Message.java
@@ -0,0 +1,19 @@
+package org.aibo.bean;
+
+import java.util.Date;
+
+/**
+ * Created by Squirrel-Chen on 2018/2/9.
+ */
+public class Message {
+
+
+    private int message_id;
+    private int message_type;
+    private int message_user_id;
+    private String message_title;
+    private String message_context;
+    private Date message_date;
+
+
+}
