c35fc85c428020036a0566d1c5d76dacbe6760f9
squirrelschen <squirrelschen@gmail.com>
2018-02-17 10:42:23 +0800

    Implements selectUserCollectedById function with serivce

--- src/main/org/aibo/service/UserCollectedService.java
+++ src/main/org/aibo/service/UserCollectedService.java
@@ -1,7 +1,11 @@
 package org.aibo.service;
 
+import org.aibo.bean.User_Collected;
+
 /**
  * Created by Squirrel-Chen on 2018/2/17.
  */
 public interface UserCollectedService {
+
+    public User_Collected selectUserCollectedById(int user_id);
 }
--- src/main/org/aibo/service/serviceimpl/UserCollectedServiceImpl.java
+++ src/main/org/aibo/service/serviceimpl/UserCollectedServiceImpl.java
@@ -1,9 +1,22 @@
 package org.aibo.service.serviceimpl;
 
+import org.aibo.bean.User_Collected;
+import org.aibo.dao.UserCollectedDao;
 import org.aibo.service.UserCollectedService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by Squirrel-Chen on 2018/2/17.
  */
+@Service
 public class UserCollectedServiceImpl implements UserCollectedService{
+
+    @Autowired
+    private UserCollectedDao userCollectedDao;
+
+
+    public User_Collected selectUserCollectedById(int user_id) {
+        return userCollectedDao.selectUserCollectedById(user_id);
+    }
 }
