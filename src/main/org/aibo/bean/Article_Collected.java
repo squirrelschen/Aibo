543b9526c89b7e4c864aac3e13715a8e579a34ae
squirrelschen <squirrelschen@gmail.com>
2018-02-19 15:55:38 +0800

    Modified User_Collected and Article_Collected javabean

---
+++ src/main/org/aibo/bean/Article_Collected.java
@@ -0,0 +1,45 @@
+package org.aibo.bean;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by Squirrel-Chen on 2018/2/7.
+ */
+public class Article_Collected implements Serializable{
+
+    private static final long serialVersionUID = 8918416247831526099L;
+
+    private int collected_id ;
+    private Article article;
+    private Date collected_date;
+
+    /**
+     *  Article_Collection getter setter 方法
+     */
+
+    public Date getCollected_date() {
+        return collected_date;
+    }
+
+    public int getCollected_id() {
+        return collected_id;
+    }
+
+    public Article getArticle() {
+        return article;
+    }
+
+    public void setCollected_date(Date collected_date) {
+        this.collected_date = collected_date;
+    }
+
+    public void setCollected_id(int collected_id) {
+        this.collected_id = collected_id;
+    }
+
+    public void setArticle(Article article) {
+        this.article = article;
+    }
+}
+
--- src/main/org/aibo/bean/User_Collected.java
+++ src/main/org/aibo/bean/User_Collected.java
@@ -2,7 +2,6 @@ package org.aibo.bean;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Date;
 
 /**
  * Created by Squirrel-Chen on 2018/2/17.
@@ -11,9 +10,7 @@ public class User_Collected implements Serializable{
 
     private static final long serialVersionUID = -3877634378182876960L;
     private int user_id;
-    private ArrayList<Article> collected_article;
-    private Date collected_date;
-
+    private ArrayList<Article_Collected> collecteds;
 
     /**
      *  User_Collection getter setter 方法
@@ -23,23 +20,15 @@ public class User_Collected implements Serializable{
         return user_id;
     }
 
-    public ArrayList<Article> getCollected_article() {
-        return collected_article;
-    }
-
-    public Date getCollected_date() {
-        return collected_date;
-    }
-
     public void setUser_id(int user_id) {
         this.user_id = user_id;
     }
 
-    public void setCollected_article(ArrayList<Article> collected_article) {
-        this.collected_article = collected_article;
+    public ArrayList<Article_Collected> getCollecteds() {
+        return collecteds;
     }
 
-    public void setCollected_date(Date collected_date) {
-        this.collected_date = collected_date;
+    public void setCollecteds(ArrayList<Article_Collected> collecteds) {
+        this.collecteds = collecteds;
     }
 }
