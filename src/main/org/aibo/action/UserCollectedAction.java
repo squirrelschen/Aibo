f961ba2267b415063a7b1298231afd9f3bc6271c
squirrelschen <squirrelschen@gmail.com>
2018-02-26 16:48:21 +0800

    Create usercollected action request mapping

--- src/main/org/aibo/action/UserCollectedAction.java
+++ src/main/org/aibo/action/UserCollectedAction.java
@@ -1,8 +1,22 @@
 package org.aibo.action;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by Squirrel-Chen on 2018/2/24.
  *  用户收藏的action
  */
+
+@Controller
 public class UserCollectedAction {
+
+
+    @RequestMapping("/collected")
+    public Object collected(){
+        return null;
+    }
+
+
+
 }
